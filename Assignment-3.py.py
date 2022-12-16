4bd35c9fc2c6d5d43d1b03ad1157e3c0d7a33f5b
neithang <120584443+neithang@users.noreply.github.com>
2022-12-16 22:10:23 +0530

    Add files via upload

---
+++ Assignment-3.py.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python program to print Even Numbers in a List
+
+# In[1]:
+
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ 
+for num in list1:
+ 
+    if num % 2 == 0:
+        print(num, end=" ")
+
+
+# # Python program to print odd Numbers in a List
+
+# In[8]:
+
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ 
+for num in list1:
+ 
+    if num % 2 != 0:
+        print(num, end=" ")
+
+
+# In[ ]:
+
+
+
+
